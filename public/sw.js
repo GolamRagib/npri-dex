49aeaa04d2f3c9d6bc44f3dfe57d93bd214b1f23
G Ferdous Ragib <g.ragib@gmail.com>
2017-04-22 14:53:15 -0400

    service worker edits

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,33 @@
-importScripts( '/cache-polyfill.js' );
-self.addEventListener( 'install', function( e ) {
-  e.waitUntil( caches.open( 'npri-dex' ).then( function( cache ) {
-    return cache.addAll( [
-      '/',
-      '/bundle.js',
-      '/cache-polyfill.js',
-      '/favicon.ico',
-      '/index.html',
-      '/manifest.json',
-      '/styles.css',
-      '/images/launcher-icon-0-75x.png',
-      '/images/launcher-icon-1-5x.png',
-      '/images/launcher-icon-1x.png',
-      '/images/launcher-icon-2x.png',
-      '/images/launcher-icon-3x.png',
-      '/images/launcher-icon-3-5x.png',
-      '/images/launcher-icon-4x.png',
-      '/images/launcher-icon-8x.png',
-      '/images/launcher-icon.png',
-      '/images/icons/favicon-16x16.png',
-      '/images/icons/favicon-32x32.png',
-      '/images/icons/favicon-96x96.png',
-      ] );
-    } )
-  );
-} );
-self.addEventListener( 'fetch', function( event ) {
-  event.respondWith( caches.match( event.request )
-                     .then( ( response ) => ( response || fetch( event.request ) ) ) );
-} );
+let swCache = 'npri-dex';
+
+let localAssets = [ '/',
+                    '/bundle.js',
+                    '/index.html',
+                    '/manifest.json',
+                    '/styles.css', ];
+
+let CDNAssets = [ 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/images/layers.png',
+                  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/images/layers-2x.png',
+                  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/images/marker-icon.png',
+                  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/images/marker-icon-2x.png',
+                  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/images/marker-shadow.png',
+                  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/leaflet.css',
+                  'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
+                  'https://fonts.googleapis.com/icon?family=Material+Icons', ];
+
+self.addEventListener( 'install',
+                       ( evt ) =>
+                         evt.waitUntil( caches.open( swCache )
+                                        .then( ( cache ) =>
+                                                 cache.addAll( localAssets ) ),
+                                        CDNAssets.map( ( request ) =>
+                                                         fetch( request, { mode: 'no-cors' } )
+                                                         .then( ( response ) =>
+                                                                  caches.open( swCache )
+                                                                  .then( ( cache ) =>
+                                                                           cache.put( request , response ) ) ) ) ) );
+
+self.addEventListener( 'fetch', ( event ) =>
+                                  event.respondWith( caches.match( event.request )
+                                                     .then( ( response ) =>
+                                                              ( response || fetch( event.request ) ) ) ) );
